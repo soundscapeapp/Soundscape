c976739694d97735c3d2e6064ed27a74d986f67e
Amanda Reynolds <amandagfreynolds@gmail.com>
2016-12-05 17:33:01 -0500

    with routes, they dont work

--- public/js/route.js
+++ public/js/route.js
@@ -3,17 +3,17 @@ var app = angular.module("soundscape", ["ngRoute"]);
 
 app.config(function($routeProvider) {
       $routeProvider.when("/", {
-        templateUrl: "index.html",
+        templateUrl: "../index.html",
         // controller: 'getController'
       
     });
     $routeProvider.when("/scapes", {
-        templateUrl: "views/scapes.html",
+        templateUrl: "../views/scapes.html",
         // controller: 'returnController'
       
     });
      $routeProvider.when("/add-scape", {
-        templateUrl: "views/add-scapes.html",
+        templateUrl: "../views/add-scape.html",
         // controller: 'returnController'
       
     });
