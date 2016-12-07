6706d258a72299563376715eb9c3be1e62266ca9
Colin Walbridge <cwalbrid@umich.edu>
2016-12-07 10:37:41 -0500

    adjusted setController to clear out savedPlaylist object after printing to the console

--- public/js/app.js
+++ public/js/app.js
@@ -14,23 +14,23 @@ app.controller('getController', function($scope, plStore){
 	
 	//image upload
 
-	imgUpload = function(){
-		$(":file").change(function () {
-	        if (this.files && this.files[0]) {
-	            var reader = new FileReader();
-	            reader.onload = imageIsLoaded;
-	            reader.readAsDataURL(this.files[0]);
-	        }
+	// imgUpload = function(){
+	// 	$(":file").change(function () {
+	//         if (this.files && this.files[0]) {
+	//             var reader = new FileReader();
+	//             reader.onload = imageIsLoaded;
+	//             reader.readAsDataURL(this.files[0]);
+	//         }
 	    
 	
 
-	function imageIsLoaded(e) {
-	    $('#myImg').attr('src', e.target.result);
-	};
+	// function imageIsLoaded(e) {
+	//     $('#myImg').attr('src', e.target.result);
+	// };
 
-	});
+	// });
 
-	imgUpload();	
+	// imgUpload();	
 
 
 
@@ -38,31 +38,18 @@ app.controller('getController', function($scope, plStore){
 	//contoller stuff 
 
 	$scope.savePlaylist = function(playlist){
-	var savedPlaylist = $scope.source.img;
+	var savedPlaylist = $scope.source;
 	// console.log(savedPlaylist);
 	plStore.savePl(savedPlaylist);
 	};
 
-	// $scope.printPlaylist = function($scope, plStore){
-	// 	var savedPlaylist = plStore.fetchPl();
-	// 	console.log(savedPlaylist);
-	//  alert('yo yo yo')
-	// };
-  // });
+	});	
 
-app.controller('setController', function($scope, plStore){
+	app.controller('setController', function($scope, plStore){
  	var savedPlaylist = plStore.fetchPl();
- 	
- 	// var testArray = [
- 	// {
- 	// 	artist: "Led Zeppelin",
- 	// 	song: "Dazed and Confused",
- 	// 	playlist: "Classic Rock"
- 	// }
- 	// ];
-
  	testArray.push(savedPlaylist);
  	console.log(testArray);
+ 	plStore.clearPl();
 });
 
 
@@ -74,11 +61,14 @@ var savedPlaylist = {};
 return {
 	savePl: function(pl){
 		savedPlaylist = pl;
-		// console.log(savedPlaylist);
+		console.log(savedPlaylist);
 	},
 	fetchPl: function() {
 		return savedPlaylist;
 		// console.log ('fetchPl');
+	},
+	clearPl: function(){
+		savedPlaylist = null;
 	}
 
 };
